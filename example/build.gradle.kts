7977fd41634b0f73fd279062ccdcad05ac7a8455
tomwmth <tomwmth@pm.me>
2026-04-19 05:21:44 +0800

    fix: pull example dependency from project

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -3,5 +3,5 @@ version = "1.0.0"
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
-    compileOnly("com.alpineclient:ridge-api:1.4.+")
+    compileOnly(project(":ridge-api"))
 }
